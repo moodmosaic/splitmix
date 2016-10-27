a32d0c41847890f0395080f2f1ad1950e661f9ec
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2016-10-27 08:47:51 +0300

    Add comments.

--- Splittable.fs
+++ Splittable.fs
@@ -1,21 +1,29 @@
+/// "Fast Splittable Pseudorandom Number Generators,
+/// Guy L. Steele Jr., Doug Lea, Christine H. Flood"
 module Splittable =
     type SplitMix =
         { Seed : int64
           Gamma : int64 }
         member this.NextSeed() = { this with Seed = this.Seed + this.Gamma }
 
+    /// Mix the bits of a 64-bit arg to produce a result, computing a
+    /// bijective function on 64-bit values.
     let mix64 (x : SplitMix) : int64 =
         let z = x.Seed
         let z = (z ^^^ (z >>> 33)) * 0xff51afd7ed558ccdL
         let z = (z ^^^ (z >>> 33)) * 0xc4ceb9fe1a85ec53L
         z ^^^ (z >>> 33)
 
+    /// Mix the bits of a 64-bit arg to produce a result, computing a
+    /// bijective function on 64-bit values.
     let mix32 (x : SplitMix) : int =
         let z = x.Seed
         let z = (z ^^^ (z >>> 33)) * 0xff51afd7ed558ccdL
         let z = (z ^^^ (z >>> 33)) * 0xc4ceb9fe1a85ec53L
         (int) (z >>> 32)
 
+    /// Mix the bits of a 64-bit arg to produce a result, computing a
+    /// bijective function on 64-bit values.
     let mix64variant13 (x : SplitMix) : int64 =
         let z = x.Seed
         let z = (z ^^^ (z >>> 30)) * 0xbf58476d1ce4e5b9L
@@ -31,14 +39,24 @@ module Splittable =
         let n = n + (n >>> 32)
         (int n) &&& 0x7f
 
+    /// Mix the bits of a 64-bit arg to produce a result, computing a
+    /// bijective function on 64-bit values.
     let mixGamma (x : SplitMix) : int64 =
         let z = mix64variant13 x ||| 1L
         let n = bitCount (z ^^^ (z >>> 1))
         if n < 24 then z ^^^ 0xaaaaaaaaaaaaaaaaL
         else z
 
+    /// The value 'DOUBLE_ULP' is the positive difference between 1.0
+    /// and the smallest double value larger than 1.0; it is used for
+    /// deriving a double value from a 64-bit long value.
     let doubleUlp : float = 1.0 / double (1L <<< 53)
 
+    /// A predefined gamma value's needed for initializing the "root"
+    /// instances of SplittableRandom that is, instances not produced
+    /// by splitting an already existing instance. We choose: the odd
+    /// integer closest to 2^64/φ, where φ = (1 + √5)/2 is the golden
+    /// ratio, and call it GOLDEN_GAMMA.
     [<Literal>]
     let goldenGamma : int64 = 0x9e3779b97f4a7c15L
 
