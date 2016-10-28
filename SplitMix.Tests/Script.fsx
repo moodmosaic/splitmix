15ada1e0e7ac23ca461ea94e75c273c5b5a6dc2e
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2016-10-28 12:57:09 +0300

    Rename Library1.fs to Scenarios.fs.

--- SplitMix.Tests/Script.fsx
+++ SplitMix.Tests/Script.fsx
@@ -1,7 +1,7 @@
 ﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
-#load "Library1.fs"
+#load "Scenarios.fs"
 open SplitMix.Tests
 
 // Define your library scripting code here
