f1b826f32885ee4880740d8027dbd1486382014c
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2016-10-29 09:01:25 +0300

    Use structures instead of records,
    since we do a lot of copying, and it might be better to just allocate these
    in the thread stack (premature optimization(?)).
    
    In a future version of the language, it'd be possible to just annotate a
    record with [<Sturct>] and it'll be turned into a (guess what) structure
    as reported here: https://fslang.uservoice.com/forums/245727-f-language/suggestions/6547517-record-types-can-be-marked-with-the-struct-attribu

--- SplitMix/SplitMix.fs
+++ SplitMix/SplitMix.fs
@@ -4,9 +4,9 @@ namespace SplittableRandom
 /// Guy L. Steele Jr., Doug Lea, Christine H. Flood"
 /// http://2014.splashcon.org/event/oopsla2014-fast-splittable-pseudorandom-number-generators
 
-type SplitMix =
-    internal { Seed : int64
-               Gamma : int64 }
+type [<Struct>] SplitMix internal (seed : int64, gamma : int64) =
+    member this.Seed = seed
+    member this.Gamma = gamma
 
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module SplitMix =
@@ -18,29 +18,28 @@ module SplitMix =
     let [<Literal>] private goldenGamma : int64 = 0x9e3779b97f4a7c15L
 
     let ofSeed (seed : int64) : SplitMix =
-        { Seed = seed
-          Gamma = goldenGamma }
+        SplitMix (seed, goldenGamma)
 
     /// Mix the bits of a 64-bit arg to produce a result, computing a
     /// bijective function on 64-bit values.
-    let private mix64 (x : SplitMix) : int64 =
-        let z = x.Seed
+    let private mix64 (x : int64) : int64 =
+        let z = x
         let z = (z ^^^ (z >>> 33)) * 0xff51afd7ed558ccdL
         let z = (z ^^^ (z >>> 33)) * 0xc4ceb9fe1a85ec53L
         z ^^^ (z >>> 33)
 
     /// Mix the bits of a 64-bit arg to produce a result, computing a
     /// bijective function on 64-bit values.
-    let private mix32 (x : SplitMix) : int =
-        let z = x.Seed
+    let private mix32 (x : int64) : int =
+        let z = x
         let z = (z ^^^ (z >>> 33)) * 0xff51afd7ed558ccdL
         let z = (z ^^^ (z >>> 33)) * 0xc4ceb9fe1a85ec53L
         (int) (z >>> 32)
 
     /// Mix the bits of a 64-bit arg to produce a result, computing a
     /// bijective function on 64-bit values.
-    let private mix64variant13 (x : SplitMix) : int64 =
-        let z = x.Seed
+    let private mix64variant13 (x : int64) : int64 =
+        let z = x
         let z = (z ^^^ (z >>> 30)) * 0xbf58476d1ce4e5b9L
         let z = (z ^^^ (z >>> 27)) * 0x94d049bb133111ebL
         z ^^^ (z >>> 31)
@@ -56,34 +55,27 @@ module SplitMix =
 
     /// Mix the bits of a 64-bit arg to produce a result, computing a
     /// bijective function on 64-bit values.
-    let private mixGamma (x : SplitMix) : int64 =
+    let private mixGamma (x : int64) : int64 =
         let z = mix64variant13 x ||| 1L
         let n = bitCount (z ^^^ (z >>> 1))
         if n < 24 then z ^^^ 0xaaaaaaaaaaaaaaaaL
         else z
 
-    let ofRandomSeed =
-        fun () ->
-            let randomSeed =
-                System.DateTimeOffset.UtcNow.Ticks + 2L * goldenGamma
-            let dummyGamma =
-                0L
-            { Seed =
-                  mix64 { Seed = randomSeed
-                          Gamma = dummyGamma }
-              Gamma =
-                  mixGamma { Seed = randomSeed + goldenGamma
-                             Gamma = dummyGamma } }
+    let ofRandomSeed () =
+        let x = System.DateTimeOffset.UtcNow.Ticks + 2L * goldenGamma
+        SplitMix (mix64 x, mixGamma x + goldenGamma)
 
     let private nextSeed (x : SplitMix) : SplitMix =
-        { x with Seed = x.Seed + x.Gamma }
+        SplitMix (x.Seed + x.Gamma, x.Gamma)
 
     let split (x : SplitMix) : SplitMix =
-        { Seed = mix64 <| nextSeed x
-          Gamma = mixGamma <| nextSeed x }
+        let y : SplitMix = x |> nextSeed
+        let z : SplitMix = y |> nextSeed
+        SplitMix (mix64 y.Seed, mixGamma z.Gamma)
 
-    let nextInt32 (x : SplitMix) : int32 = mix32 <| nextSeed x
-    let nextInt64 (x : SplitMix) : int64 = mix64 <| nextSeed x
+    let nextInt32 (x : SplitMix) : int32 = mix32 <| (nextSeed x).Seed
+    let nextInt64 (x : SplitMix) : int64 = mix64 <| (nextSeed x).Seed
+
     let nextFloat (x : SplitMix) : float =
         /// The value 'DOUBLE_ULP' is the positive difference between
         /// 1.0 and the smallest double value, larger than 1.0; it is
